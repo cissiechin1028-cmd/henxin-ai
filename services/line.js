26d3c0940062ee7ccb2e416232337cd43225fcae
cissiechin1028-cmd <cissiechin1028@gmail.com>
2026-04-22 23:25:43 +0900

    Create line.js

---
+++ services/line.js
@@ -0,0 +1,24 @@
+const axios = require("axios");
+
+async function replyMessage(replyToken, text) {
+  await axios.post(
+    "https://api.line.me/v2/bot/message/reply",
+    {
+      replyToken,
+      messages: [
+        {
+          type: "text",
+          text: text,
+        },
+      ],
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.LINE_CHANNEL_ACCESS_TOKEN}`,
+        "Content-Type": "application/json",
+      },
+    }
+  );
+}
+
+module.exports = { replyMessage };
